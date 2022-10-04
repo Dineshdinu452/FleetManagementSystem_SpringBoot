bf3615d7bb2c74c157f8f791f68191627dd0e5f0
DINESH KUMAR <73679496+Dineshdinu452@users.noreply.github.com>
2022-10-04 19:19:45 +0530

    Add files via upload

---
+++ FleetmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fleetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetmanagementApplication.class, args);
+	}
+
+}
